70395c6d1f226b5ab40bdf9319c385a37d2b042c
inkognito123 <lukyanchuk_andrei@mail.ru>
2020-05-07 00:12:55 +0300

    add new files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PersAcc from "./components/personalAccount/personalAccount"
 import Map from "./components/map/map"
+import Hookahs from "./components/hookahs/hookahs"
 import {BrowserRouter, Route} from "react-router-dom";
 
 class App extends React.Component {
@@ -8,8 +9,9 @@ class App extends React.Component {
         return (
             <BrowserRouter>
                 <div className="wrapper">
-                    <div className="pers"><Route exact path='/pers' component={PersAcc}/></div>
+                    <div className="pers"><Route exact path='/' component={PersAcc}/></div>
                     <div className="map"><Route exact path='/map' component={Map}/></div>
+                    <div className="hook"><Route exact path='/hook' component={Hookahs}/></div>
                 </div>
             </BrowserRouter>
         );
