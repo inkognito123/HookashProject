80d2fd6f6008397bdb612c6b42bbaf4d68b5d99e
inkognito123 <lukyanchuk_andrei@mail.ru>
2020-05-24 14:26:23 +0300

    add github-pages

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import PersAcc from "./components/personalAccount/personalAccount"
 import Mapp from "./components/map/map"
 import Hookahs from "./components/hookahs/hookahs"
-import {BrowserRouter, Route} from "react-router-dom";
+import {HashRouter, Route} from "react-router-dom";
 
 class App extends React.Component {
     render() {
         return (
-            <BrowserRouter>
+            <HashRouter>
                 <div className="wrapper">
                     <div className="pers"><Route exact path='/' component={PersAcc}/></div>
                     <div className="map"><Route exact path='/map' component={Mapp}/></div>
                     <div className="hook"><Route exact path='/hook' component={Hookahs}/></div>
                 </div>
-            </BrowserRouter>
+            </HashRouter>
         );
     }
 }
